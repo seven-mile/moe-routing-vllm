fix: use float32 to calculate ppl for numerical precision

vllm/v1/spec_decode/utils.py
@@ -17,7 +17,8 @@ def is_spec_decode_unsupported(sampling_params: SamplingParams) -> bool:
             or sampling_params.logprobs is not None)
 
 
-def calc_perplexity(logits, token_ids):
+def calc_perplexity(logits: torch.Tensor, token_ids: torch.Tensor) -> torch.Tensor:
+    logits = logits.float()
     assert logits.shape[:-1] == token_ids.shape, \
         f"Logits shape {logits.shape} does not match token_ids shape {token_ids.shape}"
     loss = F.cross_entropy(logits.reshape(-1, logits.size(-1)), token_ids.reshape(-1), reduction='none')